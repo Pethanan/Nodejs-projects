d2390ba9cf4112d62c43edd13dbf9bf23a51e748
Pethanan <peth.engr@gmail.com>
2023-06-10 13:46:16 +0530

    file created to store message

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res)=>{
+    res.setHeader("Content-Type", "text/html")
+    const url = req.url;
+    const method = req.method;
+   if(url==="/")
+   {
+    res.write("<html>")
+    res.write("<head><title>About</title></head>");
+    res.write("<body>");
+    res.write("<h1>Message</h1>");
+    res.write("<form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Send</button></form>");
+    res.write("</body>");
+    res.write("</html>")
+    return res.end();
+   }
+   if(url==="/message" && method==="POST")
+   {
 
-const server = http.createServer((re, res)=>{
-    console.log("pethanan")
-    console.log(req);
+    const body = [];
+    req.on("data", (chunk)=>{
+        body.push(chunk);
+        console.log(chunk)
+        body.push(chunk)
+    });
 
+    req.on("end", ()=>{
+        const parsedBody = Buffer.concat(body).toString();
+const message = parsedBody.split("=")[1];
+        fs.writeFileSync("message.txt", message)
+
+    });
+
+res.statusCode=302;
+res.setHeader("Location", "/")
+   }   
+   
 });
 
 server.listen(3000);
