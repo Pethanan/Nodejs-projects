8f28524d2ec63672499a91da18aaf1b27f8c6911
Pethanan <peth.engr@gmail.com>
2023-06-17 20:50:32 +0530

    express installed

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const handler = require("./router")
-const server = http.createServer();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  console.log("Middleware");
+  console.log("add product page");
+  res.send(
+    '<form action="/product" method="POST"> <input type="text" name="title"><input type="number" name="size"> <button type="submit">Submit</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Middleware");
+  res.send("<h1>home</h1>");
+});
+
+app.listen(3000);
--- router.js
+++ router.js
@@ -1,45 +1,41 @@
 const fs = require("fs");
 
+const requestHandler = (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  const url = req.url;
+  const method = req.method;
 
-const requestHandler = (req, res)=>{
-    res.setHeader("Content-Type", "text/html")
-    const url = req.url;
-    const method = req.method;
-   
-   if(url==="/")
-   {
-    res.write("<html>")
+  if (url === "/") {
+    res.write("<html>");
     res.write("<head><title>About</title></head>");
     res.write("<body>");
     res.write("<h1>Message</h1>");
-    res.write("<form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Send</button></form>");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Send</button></form>'
+    );
     res.write("</body>");
-    res.write("</html>")
+    res.write("</html>");
     return res.end();
-   }
-   if(url==="/message" && method==="POST")
-   {
-
+  }
+  if (url === "/message" && method === "POST") {
     const body = [];
-    req.on("data", (chunk)=>{
-        body.push(chunk);
-        console.log(chunk)
-        body.push(chunk)
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+      body.push(chunk);
     });
 
-    req.on("end", ()=>{
-        const parsedBody = Buffer.concat(body).toString();
-const message = parsedBody.split("=")[1];
-        fs.writeFileSync("message.txt", message)
-
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
     });
 
-res.statusCode=302;
-res.setHeader("Location", "/")
-return res.end();
-}   
-   
-}
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+};
 
 module.exports = requestHandler;
 
