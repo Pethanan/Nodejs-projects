326b523bece3455872b85a33a09cb57cfb67f4ac
Pethanan <peth.engr@gmail.com>
2023-06-18 19:09:27 +0530

    message logic added

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const messageRoutes = require("./routes/message");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use(messageRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not fond<h1>");
