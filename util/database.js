48829572cb53baf3573b1b0b0c2c26f55ecabc8c
Pethanan <peth.engr@gmail.com>
2023-07-08 23:36:39 +0530

    sequelise installed

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
-const mysql = require("mysql2");
+// SQLqury DB
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "peth_node",
+//   password: "PETHmysql1@",
+// });
+
+// module.exports = pool.promise();
+
+//using sequelize
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("peth_node", "root", "PETHmysql1@", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "peth_node",
-  password: "PETHmysql1@",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
